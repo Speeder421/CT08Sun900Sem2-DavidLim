6075a5784058edafed54aade5c0a82e2f030872e
Speeder421 <davidjesselim4@gmail.com>
2025-08-31 03:58:15 +0000

    Aug 31, 2025, 3:58 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,19 @@
+function setup() {
+  new Canvas(600, 400);
+  background(250);
+  world.gravity.y=10;
 
+
+}
+
+function draw() {
+    background(250);
+    if(mouse.presses()){
+    ball=new Sprite(mouse.x,mouse.y,40,'dynamic');
+    
+   
+  }
+
+  // write your codes here
+
+  }
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,25 @@
+function setup() {
+  new Canvas(600, 400);
+  background(250);
+  floor=new Sprite();
+  floor.x=300;
+  floor.y=400;
+  floor.width=600;
+  floor.height=30;
+  floor.collider="static";
+  world.gravity.y=10;
+// 
+  
+  }
 
+
+
+
+
+function draw() {
+    background(250);
+    if(mouse.presses()){
+        new Sprite(mouse.x,mouse.y,40,'dynamic');
+        
+  }
+}
